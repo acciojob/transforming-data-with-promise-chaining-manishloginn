dd6fca309ac35ab33e149ee7b6b140e9e4b4b8f6
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-27 19:47:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //your JS code here. If required.
+
+  const btn = document.getElementById("btn")
+
+  const output = document.getElementById("output")
+
+  btn.addEventListener("click", ()=> {
+    const ip = document.getElementById("ip").value;
+    const firstPromise = new Promise ((resolve, reject) => {
+      setTimeout(()=> {
+        resolve( `Result: ${ip}`)
+      }, 2000)
+    })
+   
+    const secondPromise = firstPromise.then(number => {
+      return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+          resolve( number * 2) 
+        }, 1000);
+      })
+    }) 
+
+    const thirdPromise = secondPromise.then (number => {
+      return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+          resolve( number - 3) 
+        }, 1000)
+      })
+    })
+
+    const fourthPromise = thirdPromise.then (number => {
+      return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+          resolve( number / 2) 
+        }, 1000);
+      })
+    })
+
+    const fifthPromise = fourthPromise.then (number => {
+      return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+          resolve( number + 10) 
+        }, 1000);
+      })
+    })
+
+    fifthPromise.then (finalResult => {
+      output.innerText = `Final Result: ${finalResult}`
+    })
+  })
