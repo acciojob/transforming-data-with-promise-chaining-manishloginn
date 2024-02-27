7da26f92a318a60fd3694f0516a5aa9b1b70b7bd
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-27 19:54:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
- const btn = document.getElementById("btn")
+document.getElementById('btn').addEventListener('click', function() {
+    const inputNumber = document.getElementById('ip').value;
+    
+    // First Promise
+    const firstPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(inputNumber);
+        }, 2000);
+    });
 
-  const output = document.getElementById("output")
-
-  btn.addEventListener("click", ()=> {
-    const inputNumber = document.getElementById("ip").value;
-    const firstPromise = new Promise ((resolve, reject) => {
-      setTimeout(()=> {
-        resolve(inputNumber)
-      }, 2000)
-    })
-   
+    // Second Promise
     const secondPromise = firstPromise.then(number => {
-      return new Promise ((resolve, reject) => {
-        setTimeout(() => {
-          resolve( number * 2) 
-        }, 1000);
-      })
-    }) 
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(number * 2);
+            }, 1000);
+        });
+    });
 
-    const thirdPromise = secondPromise.then (number => {
-      return new Promise ((resolve, reject) => {
-        setTimeout(() => {
-          resolve( number - 3) 
-        }, 1000)
-      })
-    })
+    // Third Promise
+    const thirdPromise = secondPromise.then(number => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(number - 3);
+            }, 1000);
+        });
+    });
 
-    const fourthPromise = thirdPromise.then (number => {
-      return new Promise ((resolve, reject) => {
-        setTimeout(() => {
-          resolve( number / 2) 
-        }, 1000);
-      })
-    })
+    // Fourth Promise
+    const fourthPromise = thirdPromise.then(number => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(number / 2);
+            }, 1000);
+        });
+    });
 
-    const fifthPromise = fourthPromise.then (number => {
-      return new Promise ((resolve, reject) => {
-        setTimeout(() => {
-          resolve( number + 10) 
-        }, 1000);
-      })
-    })
+    // Fifth Promise
+    const fifthPromise = fourthPromise.then(number => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(number + 10);
+            }, 1000);
+        });
+    });
 
-    fifthPromise.then (finalResult => {
-      output.innerText = `Final Result: ${finalResult}`
-    })
-  })
+    // Final Result
+    fifthPromise.then(finalResult => {
+        document.getElementById('output').textContent = `Final Result: ${finalResult}`;
+    });
+});
